3600468622e9c0c3cc1e2c8b0f4e22c859bdb015
beper223 <zashalovskiy@gmail.com>
2025-06-18 16:38:30 +0200

    Auto QA HW 02

---
+++ Auto_QA/HW/HW_02.py
@@ -0,0 +1,19 @@
+# pip install selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+from datetime import datetime
+
+service = Service("c:\\dist\\chromedriver-win64\\chromedriver.exe")
+options = Options()
+driver = webdriver.Chrome(service=service, options=options)
+driver.get("https://itcareerhub.de/ru")
+sleep(3)
+about_link = driver.find_element(By.LINK_TEXT, "Новости")
+about_link.click()
+sleep(3)
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+driver.save_screenshot(f"./itcareerhub_screenshot_{timestamp}.png")
+driver.quit()
